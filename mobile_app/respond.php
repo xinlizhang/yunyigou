3722b48ab6ba85ccbdee6886b43bf1a7660f81d9
xinlizhang <314625896@qq.com>
2017-05-29 12:06:48 +0800

    a

--- mobile_app/respond.php
+++ mobile_app/respond.php
@@ -7,126 +7,92 @@
 
 <?php
 
-echo $_SERVER ['DOCUMENT_ROOT'];
 define('IN_ECS', true);
-require($_SERVER ['DOCUMENT_ROOT'] . '/includes/init.php');
-require($_SERVER ['DOCUMENT_ROOT'] . '/includes/lib_payment.php');
-require($_SERVER ['DOCUMENT_ROOT'] . '/includes/lib_order.php');
-require($_SERVER ['DOCUMENT_ROOT'] . '/includes/lib_clips.php');
-echo "1";
-require_once("alipay.config.php");
-require_once("lib/alipay_notify.class.php");
-echo "1";
-//计算得出通知验证结果
-$alipayNotify = new AlipayNotify($alipay_config);
-$verify_result = $alipayNotify->verifyNotify();
+define('ROOT_PATH', realpath(dirname(__FILE__) . '/../') . '/');
+echo ROOT_PATH;
+require(dirname(__FILE__) . '/includes/init.php');
+require(ROOT_PATH . 'includes/lib_payment.php');
+require(ROOT_PATH . 'includes/lib_order.php');
+/* 支付方式代码 */
+$pay_code = !empty($_REQUEST['code']) ? trim($_REQUEST['code']) : '';
 
+//获取首信支付方式
+if (empty($pay_code) && !empty($_REQUEST['v_pmode']) && !empty($_REQUEST['v_pstring']))
+{
+    $pay_code = 'cappay';
+}
 
-if($verify_result) {//验证成功
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //请在这里加上商户的业务逻辑程序代
+if(isset($_POST['MerRemark'])  && $_POST['MerRemark']=='epay')
+{
+    $pay_code ='epay';
+}
 
+//获取快钱神州行支付方式
+if (empty($pay_code) && ($_REQUEST['ext1'] == 'shenzhou') && ($_REQUEST['ext2'] == 'ecshop'))
+{
+    $pay_code = 'shenzhou';
+}
 
-    //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+/* 参数是否为空 */
+if (empty($pay_code))
+{
+    $msg = $_LANG['pay_not_exist'];
+}
+else
+{
+    /* 检查code里面有没有问号 */
+    if (strpos($pay_code, '?') !== false)
+    {
+        $arr1 = explode('?', $pay_code);
+        $arr2 = explode('=', $arr1[1]);
 
-    //解密（如果是RSA签名需要解密，如果是MD5签名则下面一行清注释掉）
-    $notify_data = $alipayNotify->decrypt($_POST['notify_data']);
+        $_REQUEST['code']   = $arr1[0];
+        $_REQUEST[$arr2[0]] = $arr2[1];
+        $_GET['code']       = $arr1[0];
+        $_GET[$arr2[0]]     = $arr2[1];
+        $pay_code           = $arr1[0];
+    }
 
-    echo $notify_data;
+    /* 判断是否启用 */
+    $sql = "SELECT COUNT(*) FROM " . $ecs->table('payment') . " WHERE pay_code = '$pay_code' AND enabled = 1";
+    if ($db->getOne($sql) == 0)
+    {
+        $msg = $_LANG['pay_disabled'];
+    }
+    else
+    {
+        $plugin_file = 'includes/modules/payment/' . $pay_code . '.php';
 
-    //获取支付宝的通知返回参数，可参考技术文档中服务器异步通知参数列表
+        /* 检查插件文件是否存在，如果存在则验证支付是否成功，否则则返回失败信息 */
+        if (file_exists($plugin_file))
+        {
+            /* 根据支付方式代码创建支付类的对象并调用其响应操作方法 */
+            include_once($plugin_file);
 
-    //解析notify_data
-    //注意：该功能PHP5环境及以上支持，需开通curl、SSL等PHP配置环境。建议本地调试时使用PHP开发软件
-    $doc = new DOMDocument();
-    $doc->loadXML($notify_data);
-
-    if( ! empty($doc->getElementsByTagName( "notify" )->item(0)->nodeValue) ) {
-        //商户订单号
-        $out_trade_no = $doc->getElementsByTagName( "out_trade_no" )->item(0)->nodeValue;
-        //支付宝交易号
-        $trade_no = $doc->getElementsByTagName( "trade_no" )->item(0)->nodeValue;
-        //交易状态
-        $trade_status = $doc->getElementsByTagName( "trade_status" )->item(0)->nodeValue;
-
-        if($trade_status == 'TRADE_FINISHED') {
-            //判断该笔订单是否在商户网站中已经做过处理
-            //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-            //如果有做过处理，不执行商户的业务程序
-
-            //注意：
-            //该种交易状态只在两种情况下出现
-            //1、开通了普通即时到账，买家付款成功后。
-            //2、开通了高级即时到账，从该笔交易成功时间算起，过了签约时的可退款时限（如：三个月以内可退款、一年以内可退款等）后。
-
-            echo 'TRADE_FINISHED';
-
-            echo 'out_trade_no:'.$out_trade_no;
-
-            $order = order_info(0, $out_trade_no);
-
-            if ($order) {
-                echo 'order_id:'.$order['order_id'];
-
-                $log_id = insert_pay_log($order['order_id'], $order['order_amount'], PAY_ORDER);
-                echo 'log_id:'.$log_id;
-
-                order_paid($log_id, 2);
-            }
-
-            //调试用，写文本函数记录程序运行情况是否正常
-            //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
-
-            echo "success";		//请不要修改或删除
-        }
-        else if ($trade_status == 'TRADE_SUCCESS') {
-            //判断该笔订单是否在商户网站中已经做过处理
-            //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-            //如果有做过处理，不执行商户的业务程序
-
-            //注意：
-            //该种交易状态只在一种情况下出现——开通了高级即时到账，买家付款成功后。
-
-            //调试用，写文本函数记录程序运行情况是否正常
-            //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
-
-            echo 'TRADE_SUCCESS';
-
-            echo 'out_trade_no:'.$out_trade_no;
-
-            $order = order_info(0, $out_trade_no);
-
-            echo 'order:'.$order;
-
-            if ($order) {
-                echo 'order_id:'.$order['order_id'];
-
-                $log_id = insert_pay_log($order['order_id'], $order['order_amount'], PAY_ORDER);
-                echo 'log_id:'.$log_id;
-
-                order_paid($log_id, 2);
-            }
-
-            echo "success";		//请不要修改或删除
+            $payment = new $pay_code();
+            $msg     = (@$payment->respond()) ? $_LANG['pay_success'] : $_LANG['pay_fail'];
         }
         else
         {
-            echo 'TRADE_STATUS:'.$trade_status;
+            $msg = $_LANG['pay_not_exist'];
         }
     }
-
-
-    //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
-else {
-    //验证失败
-    echo "fail";
-    echo '验证签名失败';
-    //调试用，写文本函数记录程序运行情况是否正常
-    //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
-}
+
+assign_template();
+$position = assign_ur_here();
+$smarty->assign('page_title', $position['title']);   // 页面标题
+$smarty->assign('ur_here',    $position['ur_here']); // 当前位置
+$smarty->assign('page_title', $position['title']);   // 页面标题
+$smarty->assign('ur_here',    $position['ur_here']); // 当前位置
+$smarty->assign('helps',      get_shop_help());      // 网店帮助
+
+$smarty->assign('message',    $msg);
+$smarty->assign('shop_url',   $ecs->url());
+
+$smarty->display('respond.dwt');
+
 ?>
+
 </body>
 </html>
