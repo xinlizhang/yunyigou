214a933f94ffef9da271e1b06f0976462776df20
xinlizhang <314625896@qq.com>
2017-05-29 11:49:41 +0800

    a

--- mobile_app/respond.php
+++ mobile_app/respond.php
@@ -1,18 +1,67 @@
-
-
 <!DOCTYPE HTML>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
-<?php echo ($_GET['err'] == 0) ? '成功' : '失败';?>
+
 <?php
-require($_SERVER ['DOCUMENT_ROOT'] . "/mobile_app/library/function.php");
-$url = ecmobile_url()."/payment/app_callback.php?err=".$_GET['err']."&order_id=".$_GET['order_id'];
-echo "<script language='javascript' type='text/javascript'>";
-echo "window.location.href='$url'";
-echo "< /script>";
+
+define('IN_ECS', true);
+
+require(EC_PATH . 'includes/init.php');
+require(EC_PATH . 'includes/lib_payment.php');
+require(EC_PATH . 'includes/lib_order.php');
+/* 支付方式代码 */
+$pay_code = !empty($_REQUEST['code']) ? trim($_REQUEST['code']) : '';
+
+/* 参数是否为空 */
+if (empty($pay_code))
+{
+    $msg = $_LANG['pay_not_exist'];
+}
+else
+{
+    /* 检查code里面有没有问号 */
+    if (strpos($pay_code, '?') !== false)
+    {
+        $arr1 = explode('?', $pay_code);
+        $arr2 = explode('=', $arr1[1]);
+
+        $_REQUEST['code']   = $arr1[0];
+        $_REQUEST[$arr2[0]] = $arr2[1];
+        $_GET['code']       = $arr1[0];
+        $_GET[$arr2[0]]     = $arr2[1];
+        $pay_code           = $arr1[0];
+    }
+
+    /* 判断是否启用 */
+    $sql = "SELECT COUNT(*) FROM " . $ecs->table('payment') . " WHERE pay_code = '$pay_code' AND enabled = 1";
+    if ($db->getOne($sql) == 0)
+    {
+        $msg = $_LANG['pay_disabled'];
+    }
+    else
+    {
+        $plugin_file = 'includes/modules/payment/' . $pay_code . '.php';
+
+        /* 检查插件文件是否存在，如果存在则验证支付是否成功，否则则返回失败信息 */
+        if (file_exists($plugin_file))
+        {
+            /* 根据支付方式代码创建支付类的对象并调用其响应操作方法 */
+            include_once($plugin_file);
+
+            $payment = new $pay_code();
+            $msg     = (@$payment->respond()) ? $_LANG['pay_success'] : $_LANG['pay_fail'];
+        }
+        else
+        {
+            $msg = $_LANG['pay_not_exist'];
+        }
+    }
+}
+echo $msg;
 ?>
+
 </body>
 </html>
